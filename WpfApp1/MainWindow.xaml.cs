cfc7b3e55d0aef1d59007735e401ead116884589
NAO cocoalix <cocoalix@gmail.com>
2024-06-19 15:11:31 +0900

    feat: show today date in `Time` area / `Time` エリアに今日の日付を表示
    - fix: ディレクトリ構成ちょっと変だったので修正

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IDisposable
+    {
+        public DateTime _now = DateTime.Now;
+        /// <summary>
+        /// 現在の日付をリアルタイムで表示
+        /// </summary>
+        public string Now => _now.ToShortDateString();
+
+        private Task? _delayAction = null;
+
+        public MainWindow()
+        {
+            _delayAction = new Task(() =>
+            {
+                var tomorrow = _now.AddDays(1).Date;
+                var interval = tomorrow - _now;
+                Task.Delay(interval);
+                _now = DateTime.Now;
+            });
+            _delayAction?.Start();
+
+            InitializeComponent();
+        }
+        public class Time
+        {
+            public Time()
+            {
+                this.Date = DateTime.Today;
+            }
+            public DateTime Date { get; set; }
+        }
+
+        public void Dispose()
+        {
+            _delayAction?.Dispose();
+        }
+    }
+}
