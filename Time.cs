b517c1a7e31d770cea2b8a00fff628bbdc0909ed
AiArito <akaringo4869@docomo.ne.jp>
2024-06-19 11:47:51 +0900

    Add files via upload

---
+++ Time.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips_StringFormat.Times
+{
+    public class Time
+    {
+        public Time() 
+        {
+            this.Date = DateTime.Today;
+        }
+        public DateTime Date { get; set; }
+    }
+}
